ef78757b3ca085cb2bc3d976a0c2c75dc8937ea3
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-05-23 12:25:54 +0530

    [IMP] : Improved Migration for hr_expense module and done analysis work for hr_holidays.

--- addons/hr_expense/migrations/9.0.2.0/post-migration.py
+++ addons/hr_expense/migrations/9.0.2.0/post-migration.py
@@ -8,7 +8,7 @@ import logging
 from openupgradelib import openupgrade
 logger = logging.getLogger('OpenUpgrade')
 
-def map_order_state(cr):
+def map_expense_state(cr):
     # Mapping values of state field for hr_expense
     openupgrade.map_values(
         cr, openupgrade.get_legacy_name('state'), 'state', 
@@ -16,38 +16,57 @@ def map_order_state(cr):
          ('paid', 'done'), ('cancelled', 'cancel')],
         table='hr_expense')
 
-@openupgrade.migrate()
-def migrate(cr, version):
-    map_order_state(cr)
+def hr_expense(cr):
+    # Sets hr_expense_line product values to hr_expense
     cr.execute("""
     UPDATE hr_expense h SET product_id = l.product_id, unit_amount = 
-    l.unit_amount, quantity = l.unit_quantity FROM hr_expense_line l 
+    l.unit_amount, quantity = l.unit_quantity, 
+    analytic_account_id = l.analytic_account FROM hr_expense_line l 
     WHERE l.expense_id = h.id
     """)
-
+    # Counting one2many hr_expense_line and later creating hr_expense record for it.
     cr.execute("""
     SELECT * from (SELECT expense_id, COUNT(expense_id) AS "no_of_expenses", 
     case when COUNT(expense_id)>1 then true else null end as "consider" 
     FROM hr_expense_line GROUP BY expense_id) as voila where consider is not null
     """)
-    a=cr.dictfetchall()
-    for x in a:
+    expense_count=cr.dictfetchall()
+    for x in expense_count:
         expense = x['expense_id']
         no_of_expense = x['no_of_expenses']
         cr.execute("""
             select id from hr_expense_line where expense_id = %s
             """ %expense)
-        e=cr.fetchall()
-        line_ids = [n[0] for n in e[1:]]
-        g=cr.fetchall()
+        expense_line_ids=cr.fetchall()
+        line_ids = [n[0] for n in expense_line_ids[1:]]
         for z,p in zip(range(no_of_expense-1),line_ids):
-            #    analytic_account_id, payment_mode, description
             cr.execute("""
             INSERT INTO hr_expense 
-                (company_id, currency_id, journal_id, employee_id, state, date, account_move_id, name, bank_journal_id, product_uom_id, unit_amount, quantity, product_id)
-                SELECT company_id, currency_id, journal_id, employee_id, state, date, account_move_id, name, bank_journal_id, 1,
+                (company_id, currency_id, journal_id, employee_id, state, 
+                date, account_move_id, name, bank_journal_id, 
+                untaxed_amount, payment_mode, analytic_account_id, 
+                create_date, write_date, create_uid, write_uid, 
+                product_uom_id, unit_amount, quantity, product_id)
+                SELECT company_id, currency_id, journal_id, employee_id, 
+                state, date, account_move_id, name, bank_journal_id, 0.00, 'own_account', 
+                (select analytic_account from hr_expense_line where id = %(a)s),
+                (select create_date from hr_expense_line where id = %(a)s),
+                (select write_date from hr_expense_line where id = %(a)s),
+                (select create_uid from hr_expense_line where id = %(a)s),
+                (select write_uid from hr_expense_line where id = %(a)s),
+                (select uom_id from hr_expense_line where id = %(a)s),
                 (select unit_amount from hr_expense_line where id = %(a)s),
-                (select quantity from hr_expense_line where id = %(a)s),
+                (select unit_quantity from hr_expense_line where id = %(a)s),
                 (select product_id from hr_expense_line where id = %(a)s)
                 FROM hr_expense where id = %(b)s
-            """ %{'a' : z, 'b':expense})
+            """ %{'a' : p, 'b': expense})
+    cr.execute("""
+        UPDATE hr_expense h SET total_amount = 
+        (select cast(round(unit_amount*quantity) as 
+        decimal(18,2)) from hr_expense e where e.id=h.id)
+    """)
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    map_expense_state(cr)
+    hr_expense(cr)
--- addons/hr_expense/migrations/9.0.2.0/pre-migration.py
+++ addons/hr_expense/migrations/9.0.2.0/pre-migration.py
@@ -26,21 +26,8 @@ table_renames = [
     ('hr_expense_expense', 'hr_expense'),
     ]
 
-#column_drops = [
-#    ('project_config_settings', 'module_sale_service'),
-#    ('project_config_settings', 'module_pad'),
-#    ('project_config_settings', 'module_project_issue_sheet'),
-#    ('project_config_settings', 'group_time_work_estimation_tasks'),
-#    ('project_config_settings', 'module_project_timesheet'),
-#    ]
-
-
 @openupgrade.migrate()
 def migrate(cr, version):
     openupgrade.rename_tables(cr, table_renames)
     openupgrade.rename_columns(cr, column_renames)
     openupgrade.copy_columns(cr, column_copies)
-#    if openupgrade.column_exists(cr, 'project_project', 'members'):
-#        openupgrade.rename_columns(cr, {'project_project': [('members', None)]})
-#    # Removing transient tables to get rid of warnings
-#    openupgrade.drop_columns(cr, column_drops)
